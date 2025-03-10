b5e5608cdee5663f527861ff1ff674fe6f180047
dayul <rainbow9517@naver.com>
2025-03-11 00:10:17 +0900

    java baekjoon: 1351_Infinite_Sequence - 14252kb, 104ms

---
+++ java/Baekjoon/Infinite_Sequence_1351.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Infinite_Sequence_1351 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+
+    }
+}
